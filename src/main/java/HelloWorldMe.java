ed4e29850f994cc432214982007db623ed50d2ce
himanshu.mistri <himanshu.mistri@einfochips.com>
2025-09-12 19:04:14 +0530

    Fixed issue of gradle build.
    Signed-off-by: himanshu.mistri <himanshu.mistri@einfochips.com>

---
+++ src/main/java/HelloWorldMe.java
@@ -0,0 +1,9 @@
+
+
+public class HelloWorldMe {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        HelloWorld mHelloWorld = new HelloWorld();
+        mHelloWorld.print();
+    }
+}
